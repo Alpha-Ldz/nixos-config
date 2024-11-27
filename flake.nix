50143ec9235d34df076e9ebf7463661f27016301
Pierre-Louis Landouzi <pierre-louis.landouzi@epita.fr>
2024-11-27 17:24:25 +0100

    Add: sleeper config not tested

--- flake.nix
+++ flake.nix
@@ -1,40 +1,62 @@
 {
-  description = "NixOS configuration for Peuleu";
-  inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
-    home-manager.url = "github:nix-community/home-manager/release-24.05";
-    home-manager.inputs.nixpkgs.follows = "nixpkgs";
-    hyprland.url = "github:hyprwm/Hyprland";
-  };
-  outputs = inputs @ {
-    self,
-    nixpkgs,
-    home-manager,
-    ...
-  }: {
-    nixosConfigurations = {
-      laptop = let
-        username = "peuleu";
-	specialArgs = {inherit username inputs;};
-      in
-        nixpkgs.lib.nixosSystem {
-	  inherit specialArgs;
-	  system = "x86_64-linux";
+	description = "NixOS configuration for Peuleu";
+	inputs = {
+		nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
+		home-manager.url = "github:nix-community/home-manager/release-24.05";
+		home-manager.inputs.nixpkgs.follows = "nixpkgs";
+		hyprland.url = "github:hyprwm/Hyprland";
+	};
+	outputs = inputs @ {
+		self,
+			nixpkgs,
+			home-manager,
+			...
+	}: {
+		nixosConfigurations = {
+			laptop = let
+				username = "peuleu";
+			specialArgs = {inherit username inputs;};
+			in
+				nixpkgs.lib.nixosSystem {
+					inherit specialArgs;
+					system = "x86_64-linux";
 
-          modules = [
-            ./hosts/laptop
-	    ./users/${username}/nixos.nix
+					modules = [
+						./hosts/laptop
+							./users/${username}/nixos.nix
 
-	    home-manager.nixosModules.home-manager
-	    {
-              home-manager.useGlobalPkgs = true;
-              home-manager.useUserPackages = true;
+							home-manager.nixosModules.home-manager
+							{
+								home-manager.useGlobalPkgs = true;
+								home-manager.useUserPackages = true;
 
-              home-manager.extraSpecialArgs = inputs // specialArgs;
-              home-manager.users.${username} = import ./users/${username}/home.nix;
-	    }
-          ];
-        };
-    };
-  };
+								home-manager.extraSpecialArgs = inputs // specialArgs;
+								home-manager.users.${username} = import ./users/${username}/home.nix;
+							}
+					];
+				};
+			sleeper = let
+				username = "peuleu";
+			specialArgs = {inherit username inputs;};
+			in
+				nixpkgs.lib.nixosSystem {
+					inherit specialArgs;
+					system = "x86_64-linux";
+
+					modules = [
+						./hosts/sleeper
+							./users/${username}/nixos.nix
+
+							home-manager.nixosModules.home-manager
+							{
+								home-manager.useGlobalPkgs = true;
+								home-manager.useUserPackages = true;
+
+								home-manager.extraSpecialArgs = inputs // specialArgs;
+								home-manager.users.${username} = import ./users/${username}/home.nix;
+							}
+					];
+				};
+		};
+	};
 }
