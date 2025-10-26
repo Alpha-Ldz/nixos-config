5fc7f8cc24eccd79f7e2d54d417ea1cc3b04b14e
Pierre-Louis Landouzi <pierre-louis.landouzi@sopht.com>
2025-10-26 16:56:46 +0100

    feature: add rpi5 config

--- hosts/rpi5/default.nix
+++ hosts/rpi5/default.nix
@@ -9,6 +9,22 @@
   time.timeZone = "Europe/Paris";
 
   services.openssh.enable = true;
+  
+  services.k3s = {
+    enable = true;
+    role = "server";
+    tokenFile = /var/lib/rancher/k3s/server/token;
+    extraFlags = toString ([
+	    "--write-kubeconfig-mode \"0644\""
+	    "--cluster-init"
+	    "--disable servicelb"
+	    "--disable traefik"
+	    "--disable local-storage"
+    ] ++ (if meta.hostname == "rpi5" then [] else [
+	      "--server https://rpi5:6443"
+    ]));
+    clusterInit = (meta.hostname == "rpi5");
+  };
 
   system.stateVersion = "24.11";
 }
