d27529b3483f2ebbad6dc8f6410812d280cd3123
Pierre-Louis Landouzi <pierre-louis.landouzi@sopht.com>
2025-10-26 15:33:16 +0100

    feature: add rpi5 config

--- flake.nix
+++ flake.nix
@@ -1,5 +1,6 @@
 {
-  description = "NixOS configuration for Peuleu";
+  description = "Unified NixOS + Home Manager configuration for Peuleu";
+
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixpkgs-unstable";
@@ -9,30 +10,83 @@
       url = "github:nix-community/home-manager/release-24.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
     nixvim = {
       url = "github:nix-community/nixvim/nixos-24.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
+
   outputs = inputs @ {
     self,
     nixpkgs,
+    nixpkgs-unstable,
     home-manager,
     nixvim,
-    nixpkgs-unstable,
+    hyprland,
     ...
   }: let
-     system = "aarch64-linux";
-     myuser = "peuleu_server";
-     pkgs = nixpkgs.legacyPackages.${system};
+    inherit (nixpkgs) lib;
   in {
+    ## ======================
+    ## 🖥️  NixOS Configurations
+    ## ======================
+    nixosConfigurations = {
+      laptop = let
+        username = "peuleu";
+        specialArgs = { inherit username inputs; };
+      in lib.nixosSystem {
+        inherit specialArgs;
+        system = "x86_64-linux";
+
+        modules = [
+          ./hosts/laptop
+          ./users/${username}/nixos.nix
+
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.extraSpecialArgs = inputs // specialArgs;
+            home-manager.users.${username} = import ./users/${username}/home.nix;
+          }
+        ];
+      };
+
+      sleeper = let
+        username = "peuleu";
+        specialArgs = { inherit username inputs; };
+      in lib.nixosSystem {
+        inherit specialArgs;
+        system = "x86_64-linux";
+
+        modules = [
+          ./hosts/sleeper
+          ./users/${username}/nixos.nix
+
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.extraSpecialArgs = inputs // specialArgs;
+            home-manager.users.${username} = import ./users/${username}/home.nix;
+          }
+        ];
+      };
+    };
+
+    ## =======================
+    ## 🏠  Standalone Home Config
+    ## =======================
     homeConfigurations = {
-        "${myuser}" = home-manager.lib.homeManagerConfiguration {
-          inherit pkgs;
-          modules = [
-            ./test/home.nix
-          ];
+      rpi5 = home-manager.lib.homeManagerConfiguration {
+        pkgs = import nixpkgs {
+          system = "aarch64-linux";
         };
+        modules = [
+          ./test/home.nix
+        ];
+      };
     };
   };
 }
