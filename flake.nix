8081e66df12f2d3860699a59c01fb80e862745c1
Pierre-Louis Landouzi <pierre-louis.landouzi@sopht.com>
2025-10-26 14:02:15 +0100

    feature: add rpi5 config

--- flake.nix
+++ flake.nix
@@ -21,54 +21,6 @@
     nixpkgs-unstable,
     ...
   }: {
-    nixosConfigurations = {
-      laptop =
-      let
-        username = "peuleu";
-        specialArgs = {inherit username inputs;};
-      in
-        nixpkgs.lib.nixosSystem {
-          inherit specialArgs;
-          system = "x86_64-linux";
-
-          modules = [
-            ./hosts/laptop
-            ./users/${username}/nixos.nix
-
-            home-manager.nixosModules.home-manager
-            {
-              home-manager.useGlobalPkgs = true;
-              home-manager.useUserPackages = true;
-
-              home-manager.extraSpecialArgs = inputs // specialArgs;
-              home-manager.users.${username} = import ./users/${username}/home.nix;
-            }
-          ];
-        };
-
-      sleeper = let
-        username = "peuleu";
-        specialArgs = {inherit username inputs;};
-      in
-        nixpkgs.lib.nixosSystem {
-          inherit specialArgs;
-          system = "x86_64-linux";
-
-          modules = [
-            ./hosts/sleeper
-            ./users/${username}/nixos.nix
-
-            home-manager.nixosModules.home-manager
-            {
-              home-manager.useGlobalPkgs = true;
-              home-manager.useUserPackages = true;
-
-              home-manager.extraSpecialArgs = inputs // specialArgs;
-              home-manager.users.${username} = import ./users/${username}/home.nix;
-            }
-          ];
-        };
-    };
     homeConfigurations = {
       rpi5 = let
         system = "aarch64-linux";
