72ec8f5180d395b10754824a78f1945151e2bec9
Pierre-Louis Landouzi <pierre-louis.landouzi@sopht.com>
2025-11-01 21:50:20 +0100

    tmp

--- modules/k3s.nix
+++ modules/k3s.nix
@@ -9,9 +9,9 @@
 
     token = "K1037f8c0889d0f79797a21374af769228a3a33aff3f509989d67e47be0c6c6f3f8::server:b79b87d4326d97fd40d4415d1a0cf34a";
 
-    extraFlags = [
-      "--default-local-storage-path /data/k3s"
-    ];
+    # extraFlags = [
+    #  "--default-local-storage-path /data/k3s"
+    # ];
   };
   networking.firewall.allowedTCPPorts = [ 6443 10250 ];
 }
